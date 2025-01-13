1206f9023406539c92322ad01a744292dcaefe89
hunter7149 <emonnatbd@gmail.com>
2025-01-14 01:00:59 +0600

    chengine some

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -18,7 +18,7 @@ switch(statusCode)
          res.json({title:"Forbidden" ,message:err.message,stackTrace:err.stack});
         break;  
     case constants.SERVER_ERROR :
-         res.json({title:"sERVER Error" ,message:err.message,stackTrace:err.stack});
+         res.json({title:"Server Error" ,message:err.message,stackTrace:err.stack});
         break;
     default:
         console.log("No error found");
